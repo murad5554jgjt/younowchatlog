c78c650935359655af20c36b33a9571db561c446
unicorncode101 <107824809+unicorncode101@users.noreply.github.com>
2022-06-20 07:45:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -270,29 +270,3 @@ writeLogTest(outstuff);
 
 }
 
-
-function timeConverter(UNIX_timestamp){
-  var a = new Date(UNIX_timestamp * 1000);
-  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-  var year = a.getFullYear();
-  var month = months[a.getMonth()];
-  var date = a.getDate();
-  var hour = a.getHours();
-  var min = a.getMinutes();
-  var sec = a.getSeconds();
-  if(sec <10 ){
-sec ="0"+sec
-
-  }
-  if(hour <10) {
-hour = "0" + hour;
-
-  }
-  if(min <10){
-min = "0" + min
-
-  }
-  var time =   hour + ':' + min + ":"+sec;
-  return time;
-}
-
