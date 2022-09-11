72cfb2ee7fc4dd8f3a1b6ddc28c8e1dda808506f
unicorncode101 <107824809+unicorncode101@users.noreply.github.com>
2022-09-11 16:35:21 -0700

    Update yn.js

--- game/yn.js
+++ game/yn.js
@@ -5,27 +5,62 @@
 // chat log below work
 var jsonInfoData =0;
 
-
-	
-	
-var goodies = null; 
- async function getSpecialFans(userid){
- var ids =0; 
- var el3 =   document.getElementById("info2") ;
-	http://localhost:8081/info/51919855
-	var urlForInfo='http://localhost:8081/info/'+userid;
-  var json = fetch (urlForInfo)
+ async function getAllJsonInfo(userIDS){
+		var urlForInfo='http://localhost:8081/info/'+userIDS;
+		var jsonId =0;
+	var json = fetch (urlForInfo)
         .then (blob => blob.json ())
         .then (data =>
         {
             json = JSON.stringify (data, null, 2);
             jsonId = JSON.parse (json);
-			
-			
-			
-ids=jsonId.totalFans;		
-				console.log(ids);
+			//console.log(jsonId);
 
+return jsonId; 
+
+			
+        });
+
+
+	
+}
+
+ function assignReferees(moderatorUserId)
+    {
+        console.log("actionId=" + '10' + "&userId=" + publicUserId + "&onUserId=" + moderatorUserId + "&broadcastId=" + publicBroadcastId + "&broadcaster=0");
+
+var publicBroadcastId   = (getAllJsonInfo(moderatorUserId)).broadcastId 
+
+
+        fetch("//api.younow.com/php/api/doAdminAction", {
+            "headers": {
+                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "x-requested-by": localStorage.getItem("requestBy")
+            },
+            "body": "actionId=" + '10' + "&userId=" + publicUserId + "&onUserId=" + moderatorUserId + "&broadcastId=" + publicBroadcastId + "&broadcaster=0",
+            "method": "POST",
+            "mode": "cors",
+            "credentials": "include"
+        });
+    }
+	
+	
+var goodies = null; 
+async function getSpecialFans(userid){
+	
+	var ids =0; 
+	var el3 =   document.getElementById("info2") ;
+	el3.style.display="block";
+ 
+	http://localhost:8081/info/51919855
+	var urlForInfo='http://localhost:8081/info/'+userid;
+	var json = fetch (urlForInfo)
+        .then (blob => blob.json ())
+        .then (data =>
+        {
+            json = JSON.stringify (data, null, 2);
+            jsonId = JSON.parse (json);
+			ids=jsonId.totalFans;		
 			el3.innerHTML = "Total fans " +  ids;
         });
 	
@@ -35,42 +70,38 @@ ids=jsonId.totalFans;
 }
 
 async function ListCards(whoToDisplay){
-const response = await fetch('http://localhost:8080/username/'+whoToDisplay);
-const body = await response.text();
-//console.log(body); 
-var el3 =   document.getElementById("cardlist") ;
-var results=body.split('<br>'); 
-var i=0; var counts=0;
-var output =""; 
-el3.style.visibility = 'visible';
-el3.style.color= 'white';
+	const response = await fetch('http://localhost:8080/username/'+whoToDisplay);
+	const body = await response.text();
+	//console.log(body); 
+	var el3 =   document.getElementById("cardlist") ;
+	var results=body.split('<br>'); 
+	var i=0; var counts=0;
+	var output =""; 
+	el3.style.visibility = 'visible';
+	el3.style.color= 'white';
 
 
 console.log("within list cards"); 
 
-while(i < results.length){
-var temp = results[i].split(","); 
+	while(i < results.length){
+	var temp = results[i].split(","); 
 
 
-output2= "<center><h2 style='font-family: Josefin Sans;font-size:30px', sans-serif;'>"+ whoToDisplay+ " "+"</h2></center>" 
- 
-//console.log(onList)
-//var Searching = getUserIds(whoToDisplay)
-var Searching = null; 
+	output2= "<center><h2 style='font-family: Josefin Sans;font-size:30px', sans-serif;'>"+ whoToDisplay+ " "+"</h2></center>" 
+	 
+	//console.log(onList)
+	//var Searching = getUserIds(whoToDisplay)
+	var Searching = null; 
 
 
-if((temp[0].includes(whoToDisplay) )){
+		if((temp[0].includes(whoToDisplay) )){
 
-output = output+"<div style='display:inline'><div style='display:inline;'> <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/" + temp[1].trim() +".svg'  style='border-radius: 50%;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);'/></div></div> "
+		output = output+"<div style='display:inline'><div style='display:inline;'> <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/" + temp[1].trim() +".svg'  style='border-radius: 50%;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);'/></div></div> "
 
-counts++;
+		counts++;
 
-}
-else{
-
-// nothing :D   
-
-}
+		}
+		
 
 
 // console.log(temp[]);
@@ -246,8 +277,24 @@ function FetchEvent (userId){
 							
 						}
 						else if(whatsSaid.includes("!new")!=false){
+						
+							if(CommentUserId !=2282276){
+
+							}else{
+							 init(); 
+							}
+					
 							
-							init(); 
+							
+						}
+						else if(whatsSaid.includes("!show")!=false){
+						
+							if(CommentUserId !=2282276){
+
+							}else{
+							showWord();
+							}
+					
 							
 							
 						}
@@ -261,7 +308,7 @@ function FetchEvent (userId){
 						var temp = whatsSaid.split(" " ); 
 						if((temp.length > 1) ){
 						console.log("From "+username + " " 	+"sending to "+temp[1] +  "card number " +temp[2]  );
-	tradeCards(temp[1],username,temp[2])
+						tradeCards(temp[1],username,temp[2])
 						}
 						else{
 						alert("Unable to  trade missing data"); 
@@ -311,10 +358,39 @@ function FetchEvent (userId){
         channel.bind('onGift', function (data)
         {
 		
-		   playSound("./sounds/thank_you.wav"); 
+		// check who  got the gift  and display it in a different list
+		
+		playSound("./sounds/thank_you.wav"); 
 		let tempUsername = data.message.stageGifts[0].profileUrlString
 		var giftids = data.message.stageGifts[0].giftId;
-		var onlydata = data.message.stageGifts[0];
+		var onlydata = data.message.stageGifts[0] ;
+		
+		if(data.message.stageGifts[0].extraData.numOfLikes >0){
+			console.log("GOT " +data.message.stageGifts[0].receiverFirstName +" likes " +  data.message.stageGifts[0].extraData.numOfLikes)}
+		else
+		{
+			
+			if(data.message.stageGifts[0].extraData.value >0){
+				
+					console.log(data.message.stageGifts[0].extraData.value)
+			}
+			else if(data.message.stageGifts[0].giftId===897){
+				
+				displayPopUp2("New sub");
+				
+			}
+			else if(data.message.stageGifts[0].giftId!=248){
+			
+			console.log(data.message.stageGifts[0]);			
+			//248
+			}
+			else
+			{
+			
+			}
+			
+		}
+		
 		
 		if(displayGiftOnly(giftids,onlydata) != 0){
 		
